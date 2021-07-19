a0bbc3164aede797e08d5d86b85f1c2cc14a82fa
Deepak Biswal <deepak412@gmail.com>
2021-07-19 22:06:18 +0530

    Changed default port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const listen = () => {
   app.listen(PORT, () => {
     console.log(`🚀 Service API is ready to use on port ${PORT}`);
